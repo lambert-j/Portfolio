64f2957a77d34a170d78d9d4d570307a13991d0b
lambert-j <jordan.lambert.pro@gmail.com>
2023-04-21 11:02:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.querySelector("#reboot").innerHTML = addEventListener(onclick);
+
+function displayNoneRemove() {
+  let elements = document.querySelector("#reboot-box");
+  elements.classList.remove("display-none");
+}
+
+function displayNoneAddRebootBox() {
+  let elements = document.querySelector("#reboot-box");
+  elements.classList.add("display-none");
+}
