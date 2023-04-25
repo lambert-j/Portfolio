c866c76108ffc14ca90bdfb18a9d7a79ff2e5757
lambert-j <jordan.lambert.pro@gmail.com>
2023-04-25 12:08:26 +0200

    try at JS

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function displayNoneAddRebootBox() {
   let elements = document.querySelector("#reboot-box");
   elements.classList.add("display-none");
 }
+
+function sousMenu1() {
+  document.querySelector("#sous-menu-window").innerHTML = function () {
+    let imageBox = document.createElement("img");
+    imageBox.className = "image-box-sous-menu";
+    let sousMenuText = document.createElement("div");
+    sousMenuText.className = "sous-menu-text-box";
+    let sousMenuAnchor = document.createElement("a");
+    sousMenuAnchor.className = "sous-menu-link-box";
+  };
+}
