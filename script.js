804d3b4b7805f85be0edff10df7e6017ed0e57d2
lambert-j <jordan.lambert.pro@gmail.com>
2023-04-28 10:27:51 +0200

    anim menu projet done

--- script.js
+++ script.js
@@ -11,39 +11,37 @@ function displayNoneAddRebootBox() {
 }
 
 function DisplayToggleSousMenu1() {
-  let elementAll = document.querySelector(
-    ".sous-menu-window2",
-    ".sous-menu-window3",
-    ".sous-menu-window4"
-  );
-  elementAll.classList.add("display-none");
+  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
+  elementAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
   let element = document.querySelector(".sous-menu-window1");
   element.classList.remove("display-none");
 }
 
 function DisplayToggleSousMenu2() {
-  let elementAll = document.querySelector(
-    ".sous-menu-window1, .sous-menu-window3, .sous-menu-window4"
-  );
-  elementAll.classList.add("display-none");
+  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
+  elementAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
   let element = document.querySelector(".sous-menu-window2");
   element.classList.remove("display-none");
 }
 
 function DisplayToggleSousMenu3() {
-  let elementAll = document.querySelector(
-    ".sous-menu-window1, .sous-menu-window2, .sous-menu-window4"
-  );
-  elementAll.classList.add("display-none");
+  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
+  elementAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
   let element = document.querySelector(".sous-menu-window3");
   element.classList.remove("display-none");
 }
 
 function DisplayToggleSousMenu4() {
-  let elementAll = document.querySelector(
-    ".sous-menu-window1, .sous-menu-window2, .sous-menu-window3"
-  );
-  elementAll.classList.add("display-none");
+  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
+  elementAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
   let element = document.querySelector(".sous-menu-window4");
   element.classList.remove("display-none");
 }
