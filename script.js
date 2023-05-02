ed554151cfa8166ef728589e6eb2d76776e7a6d7
lambert-j <jordan.lambert.pro@gmail.com>
2023-05-02 12:09:00 +0200

    menu display done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// document.querySelector("#reboot").innerHTML = addEventListener(onclick);
-
+// Reboot box display on click
 function displayNoneRemove() {
   let elements = document.querySelector("#reboot-box");
   elements.classList.remove("display-none");
@@ -10,6 +9,32 @@ function displayNoneAddRebootBox() {
   elements.classList.add("display-none");
 }
 
+// Menu Profil Display
+function DisplayProfilMenu() {
+  let elementGlobalAll = document.querySelectorAll(".global-display");
+  elementGlobalAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
+  let elementAll = document.querySelectorAll(".profil-display");
+  elementAll.forEach((element) => {
+    element.classList.remove("display-none");
+  });
+}
+
+// Menu projets display
+
+function DisplayProjetMenu() {
+  let elementGlobalAll = document.querySelectorAll(".global-display");
+  elementGlobalAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
+  let elementAll = document.querySelectorAll(".projet-menu-display");
+  elementAll.forEach((element) => {
+    element.classList.remove("display-none");
+  });
+}
+
+// Sous Menu display
 function DisplayToggleSousMenu1() {
   let elementAll = document.querySelectorAll(".sous-menu-windows-all");
   elementAll.forEach((element) => {
@@ -44,4 +69,17 @@ function DisplayToggleSousMenu4() {
   });
   let element = document.querySelector(".sous-menu-window4");
   element.classList.remove("display-none");
+}
+
+// Menu Contacts display
+
+function DisplayContactMenu() {
+  let elementGlobalAll = document.querySelectorAll(".global-display");
+  elementGlobalAll.forEach((element) => {
+    element.classList.add("display-none");
+  });
+  let elementAll = document.querySelectorAll(".contact-display");
+  elementAll.forEach((element) => {
+    element.classList.remove("display-none");
+  });
 }
