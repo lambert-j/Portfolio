548fd2f38583e35166aa6eeda34c9381fa6d6c1e
lambert-j <jordan.lambert.pro@gmail.com>
2023-05-04 12:49:59 +0200

    animation stop menu done

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
 // Reboot box display on click
 function DisplayNoneRemove() {
+  displayNoneGlobal();
   let elements = document.querySelector("#reboot-box");
   elements.classList.remove("display-none");
 }
 
+// Fonction du bouton annuler de la reboot box
 function displayNoneAddRebootBox() {
   let elements = document.querySelector("#reboot-box");
   elements.classList.add("display-none");
+  removeNavActive();
 }
 
-// Menu Profil Display
-function DisplayProfilMenu() {
+// fonction ADD DISPLAY NONE to Global
+function displayNoneGlobal() {
   let elementGlobalAll = document.querySelectorAll(".global-display");
   elementGlobalAll.forEach((element) => {
     element.classList.add("display-none");
   });
+}
+
+// Menu Profil Display
+function DisplayProfilMenu() {
+  displayNoneGlobal();
   let elementAll = document.querySelectorAll(".profil-display");
   elementAll.forEach((element) => {
     element.classList.remove("display-none");
   });
+  removeProjetActive();
+  displayRemoveSousMenu();
 }
 
 // Menu projets display
 
 function DisplayProjetMenu() {
-  let elementGlobalAll = document.querySelectorAll(".global-display");
-  elementGlobalAll.forEach((element) => {
-    element.classList.add("display-none");
-  });
+  displayNoneGlobal();
   let elementAll = document.querySelectorAll(".projet-menu-display");
   elementAll.forEach((element) => {
     element.classList.remove("display-none");
   });
 }
+// fonction remove display sous menu windows
 
-// Sous Menu display
-function DisplayToggleSousMenu1() {
+function displayRemoveSousMenu() {
   let elementAll = document.querySelectorAll(".sous-menu-windows-all");
   elementAll.forEach((element) => {
     element.classList.add("display-none");
   });
+}
+
+// Sous Menu display
+function DisplayToggleSousMenu1() {
+  displayRemoveSousMenu();
   let element = document.querySelector(".sous-menu-window1");
   element.classList.remove("display-none");
 }
 
 function DisplayToggleSousMenu2() {
-  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
-  elementAll.forEach((element) => {
-    element.classList.add("display-none");
-  });
+  displayRemoveSousMenu();
   let element = document.querySelector(".sous-menu-window2");
   element.classList.remove("display-none");
 }
 
 function DisplayToggleSousMenu3() {
-  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
-  elementAll.forEach((element) => {
-    element.classList.add("display-none");
-  });
+  displayRemoveSousMenu();
   let element = document.querySelector(".sous-menu-window3");
   element.classList.remove("display-none");
 }
 
 function DisplayToggleSousMenu4() {
-  let elementAll = document.querySelectorAll(".sous-menu-windows-all");
-  elementAll.forEach((element) => {
-    element.classList.add("display-none");
-  });
+  displayRemoveSousMenu();
   let element = document.querySelector(".sous-menu-window4");
   element.classList.remove("display-none");
 }
@@ -82,4 +85,37 @@ function DisplayContactMenu() {
   elementAll.forEach((element) => {
     element.classList.remove("display-none");
   });
+  removeProjetActive();
+  displayRemoveSousMenu();
+}
+
+// fonction remove active projet menu
+function removeProjetActive() {
+  let elementProjetAll = document.querySelectorAll(".projet-menu-principal");
+  elementProjetAll.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
+
+// fonction ACTIVE projet menu
+function activeProjet(clicked_id) {
+  removeProjetActive();
+  let target = document.querySelector("#" + clicked_id);
+  target.classList.add("active");
+}
+
+// Remove active-nav class from NAV
+
+function removeNavActive() {
+  let elementProjetAll = document.querySelectorAll(".nav-menu-principal");
+  elementProjetAll.forEach((element) => {
+    element.classList.remove("active-nav");
+  });
+}
+
+// Fonction ACTIVE-NAV navbar
+function activeNav(clicked_id) {
+  removeNavActive();
+  let target = document.querySelector("#" + clicked_id);
+  target.classList.add("active-nav");
 }
